0defdb2bcfe2c9f3de4e97870365b21b3cde0d5d
Alan Morales <alanmg@fb.com>
2015-02-03 16:03:14 -0800

    Initial Commit. No SDKs in

---
+++ dse-parse-ios-swiftTests/dse_parse_ios_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  dse_parse_ios_swiftTests.swift
+//  dse-parse-ios-swiftTests
+//
+//  Created by Alan Morales on 2/3/15.
+//  Copyright (c) 2015 Facebook Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class dse_parse_ios_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
